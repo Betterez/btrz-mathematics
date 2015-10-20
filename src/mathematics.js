f6a7aae9281478d37465fbaa729f0018f67ace25
Pablo Brudnick <pbrudnick@fiqus.com>
2015-10-20 16:08:24 -0300

    Moving btrz-mathematics into its own repo

---
+++ src/mathematics.js
@@ -0,0 +1,108 @@
+"use strict";
+
+const DOLLAR_MULTIPLIER = 100000;
+const PERCENTAGE_MULTIPLIER = 1000;
+
+function getCorrectedPercentile(percentile) {
+  if (Math.abs(percentile) > 100) {
+    return (percentile / PERCENTAGE_MULTIPLIER);
+  }
+  return percentile;
+}
+
+function addZeroes(value, size) {
+  let parts = String(value).split('.');
+  let whole = parts[0] || '';
+  let cents = parts[1] || '';
+  size = size - cents.length;
+  let pad = '';
+  let i = 0;
+  if (whole === '0') {
+      whole = '';
+  }
+  for (i = 0; i < size; (i += 1)) {
+      pad += '0';
+  }
+  return Number(whole + cents + pad);
+}
+
+class Mathematics {
+
+  toPercentage(value) {
+    return addZeroes(this.round(value, 3), 3);
+  }
+
+  toMoney(value) {
+    return addZeroes(this.round(value, 5), 5);
+  }
+
+  percentage(base, percentile, rounding) {
+    if (!rounding) { rounding = {decimals: 2, policy: 'default'}; }
+    let fixedPercentile = getCorrectedPercentile(percentile);
+    let calculated = (base * (fixedPercentile / (100 * DOLLAR_MULTIPLIER)));
+    let rounded = this.round(calculated, rounding.decimals, null, rounding.policy);
+    return Number(this.toMoney(rounded));
+  }
+
+  roundMoney(value, digits, roundingPolicy) {
+    if (digits === undefined || digits === null || digits === '') { digits = 2; }
+    return this.round(value, digits, DOLLAR_MULTIPLIER, roundingPolicy);
+  }
+
+  round(value, digits, divisor, roundingPolicy) {
+    if (digits === undefined || digits === null) { digits = 2; }
+    if (!divisor) { divisor = 1; }
+    if (!roundingPolicy) { roundingPolicy = 'default'; }
+    let simplified = (value / divisor);
+    let parts = String(simplified).split('.');
+    let whole = parts[0] || '0';
+    let cents = parts[1] || '';
+    let centsLength = 0;
+    let roundedCents = '';
+    let flip;
+    let i = 0;
+    let d = 0;
+
+    if (cents) {
+      centsLength = cents.length;
+    }
+
+    if (digits == 0) {
+      flip = cents.substr((digits), 1);
+      if ((flip < 5) || roundingPolicy === 'down') { return whole + '.00'; }
+      if ((flip > 4) || roundingPolicy === 'up') {
+        whole = Number(whole) + 1;
+        return whole + '.00';
+      }
+    }
+
+    if (centsLength > digits) {
+      flip = cents.substr((digits), 1);
+      roundedCents = cents.substr(0, (digits));
+      if (flip > 4 && (roundingPolicy !== 'down')) {
+        roundedCents = Number(roundedCents) + 1;
+      }
+      if (String(roundedCents).length < digits) {
+        let zeroes = "";
+        for (d = 0; d < (digits - String(roundedCents).length); (d += 1)) {
+          zeroes += "0";
+        }
+        roundedCents = zeroes + roundedCents;
+      }
+      if (roundedCents === 100){
+        whole = Number(whole) + 1;
+        return whole + '.00';
+      }
+    } 
+    else {
+      roundedCents = cents;
+      for (i = centsLength; i < digits; (i += 1)) {
+        roundedCents += '0';
+      }
+    }
+
+    return whole + '.' + roundedCents;
+  }
+}
+
+exports.Mathematics = Mathematics;
---
+++ test/mathematics.js
@@ -0,0 +1,117 @@
+"use strict";
+
+describe("Mathematics", function () {
+
+  let Mathematics = require("../src/mathematics").Mathematics;
+  let expect = require("chai").expect;
+  let mathematics;
+  
+  beforeEach(function () {
+    mathematics = new Mathematics();
+  });
+
+  it("should convert to money", function () {
+    expect(mathematics.toMoney(32.30)).to.eql(3230000);
+  });
+
+  it("should convert to percentage", function () {
+    expect(mathematics.toPercentage(6.3)).to.eql(6300);
+  });
+
+  it("should calculate a percentage", function () {
+    let value = mathematics.toMoney(100);
+    let perc = mathematics.toPercentage(10);
+    expect(mathematics.percentage(value, perc)).to.eql(1000000);
+  });
+
+  it("should calculate percentage with cents", function () {
+    expect(mathematics.percentage(50000, 13000)).to.eql(7000);
+  });
+
+  it("should calculate percentage of complex values", function () {
+    expect(mathematics.percentage(3930000, 15000)).to.eql(590000);
+  });
+
+  it("should calculate complex value percentages", function () {
+    expect(mathematics.percentage(8253000, -50000)).to.eql(-4127000);
+  });
+
+  it("should calculate complex value percentages with a rounding policy", function () {
+    expect(mathematics.percentage(8253000, 50000, {decimals: 2, policy: "default"})).to.eql(4127000);
+  });
+
+  it("should calculate a ten perc on 4900000 with no dec and default policy", function () {
+    expect(mathematics.percentage(4900000, 10000, {decimals: 2, policy: "default"})).to.eql(490000);
+  });
+
+  it("should calculate a percentage of a complex value and given a rounding policy of zero decimals", function () {
+    expect(mathematics.percentage(8253000, 50000, {decimals: 0, policy: "default"})).to.eql(4100000);
+  });
+
+  it("should calculate a percentage of a complex value and given a rounding policy of zero decimals up", function () {
+    expect(mathematics.percentage(8253000, 50000, {decimals: 0, policy: "up"})).to.eql(4100000);
+  });
+
+  it("should calculate a percentage of a complex value and given a rounding policy of two decimals down", function () {
+    expect(mathematics.percentage(8989000, 50000, {decimals: 2, policy: "down"})).to.eql(4494000);
+  });
+
+  it("should calculate a percentage of a complex value and given a rounding policy of zero decimals up", function () {
+    expect(mathematics.percentage(8989000, 50000, {decimals: 0, policy: "up"})).to.eql(4500000);
+  });
+
+  it("should calculate a percentage of a complex value and given a rounding policy of zero decimals default", function () {
+    expect(mathematics.percentage(3000000, 13000, {decimals: 0, policy: "default"})).to.be.eql(400000);
+  });
+
+  it("should calculate a percentage of a complex value and given a rounding policy of zero decimals up", function () {
+    expect(mathematics.percentage(3000000, 13000, {decimals: 0, policy: "up"})).to.be.eql(400000);
+  });
+
+  it("should calculate a percentage of a complex value and given a rounding policy of zero decimals down", function () {
+    expect(mathematics.percentage(3000000, 13000, {decimals: 0, policy: "down"})).to.be.eql(300000);
+  });
+
+  it("should round with no decimals", function () {
+    expect(mathematics.roundMoney(100000)).to.be.eql("1.00");
+  });
+
+  it("should round with decimals", function () {
+    expect(mathematics.roundMoney(100000, 3)).to.be.eql("1.000");
+  });
+  it("should round with decimals a round value", function () {
+    expect(mathematics.roundMoney(1050000)).to.be.eql("10.50");
+  });
+
+  it("should round with decimals a less than zero value", function () {
+    expect(mathematics.roundMoney(50000)).to.be.eql("0.50");
+  });
+
+  it("should round with decimals a non round value", function () {
+    expect(mathematics.roundMoney(1049567)).to.be.eql("10.50");
+  });
+
+  it("should round with decimals a less than zero non round value", function () {
+    expect(mathematics.roundMoney(49567)).to.be.eql("0.50");
+  });
+
+  it("should round with precision 2 and default", function () {
+    expect(mathematics.round(49567, 2, 100000, "default")).to.be.eql("0.50");
+  });
+
+  it("should round 90 cents with precision 2 and default", function () {
+    expect(mathematics.round(99567, 2, 100000, "default")).to.be.eql("1.00");
+  });
+
+  it("should round 90 cents with precision 2 and up", function () {
+    expect(mathematics.round(99567, 2, 100000, "up")).to.be.eql("1.00");
+  });
+
+  it("should round 90 cents with precision 2 and down", function () {
+    expect(mathematics.round(99567, 2, 100000, "down")).to.be.eql("0.99");
+  });
+
+  it("should round 90 cents with precision 0 and down", function () {
+    expect(mathematics.round(99567, 0, 100000, "down")).to.be.eql("0.00");
+  });
+});
